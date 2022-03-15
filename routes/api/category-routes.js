c303b8ff3c2441e03fee390cb601ee211ad466af
Gwen Ewasko <gwenewasko@gmail.com>
2022-03-14 19:06:49 -0500

    /categories get route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,11 +4,25 @@ const { Category, Product } = require("../../models");
 // The `/api/categories` endpoint
 
 router.get("/", (req, res) => {
-  // find all categories
+  Category.findAll({
+    include: Product,
+  })
+    .then((categories) => {
+      res.json(categories);
+    })
+    .catch((err) => res.status(500).json(err));
   // be sure to include its associated Products
 });
 
 router.get("/:id", (req, res) => {
+  Category.findOne({
+    where: { id: req.params.id },
+    include: Product,
+  })
+    .then((category) => {
+      res.json(category);
+    })
+    .catch((err) => res.status(500).json(err));
   // find one category by its `id` value
   // be sure to include its associated Products
 });
